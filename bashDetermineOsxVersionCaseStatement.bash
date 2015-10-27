2667a8f8b1adfbef7179d9ba0dd2d2eb967df4cb
The Mac Admin <tma@themacadmin.com>
2015-10-26 21:49:47 -0700

    Case Statement Sample
    Added sample script

---
+++ bashDetermineOsxVersionCaseStatement.bash
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+macosxversion=`sw_vers | awk '/ProductVersion:/ { print $2 }' | cut -c -5`
+
+case $macosxversion in
+
+     10.7*)
+          echo "Lion";;
+     10.8*)
+          echo "Mountain Lion";;
+     10.9*)
+          echo "Mavericks";;
+     10.10*)
+          echo "Yosemite";;
+     10.11*)
+          echo "El Capitan";;
+     * )
+          echo "This is an unsupported version of OS X"
+          exit;;
+esac
+
+exit 0

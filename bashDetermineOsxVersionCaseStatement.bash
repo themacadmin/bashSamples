7f2ee26581eb9db36893d9efe63414a086326c47
The Mac Admin <miles@themacadmin.com>
2015-10-29 22:47:15 -0700

    Update bashDetermineOsxVersionCaseStatement.bash

--- bashDetermineOsxVersionCaseStatement.bash
+++ bashDetermineOsxVersionCaseStatement.bash
@@ -4,10 +4,6 @@ macosxversion=`sw_vers | awk '/ProductVersion:/ { print $2 }' | cut -c -5`
 
 case $macosxversion in
 
-     10.7*)
-          echo "Lion";;
-     10.8*)
-          echo "Mountain Lion";;
      10.9*)
           echo "Mavericks";;
      10.10*)
